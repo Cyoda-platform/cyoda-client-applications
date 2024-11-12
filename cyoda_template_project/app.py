8d3c1dc4dd5c086f891124bfc852cd33749e3512
Ksenniya <lukkonina1@gmail.com>
2024-11-12 13:50:52 -0300

    cyoda python template

--- cyoda_template_project/app.py
+++ cyoda_template_project/app.py
@@ -1,9 +1,27 @@
-from flask import Flask
-app = Flask(__name__)
+import asyncio
+import logging
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from quart import Quart, request
+from common.auth.auth import authenticate
+from common.grpc_client.grpc_client import grpc_stream
+from common.repository.cyoda.cyoda_init import init_cyoda
+
+app = Quart(__name__)
+token = authenticate()
+
+logging.basicConfig(level=logging.INFO)
+
+@app.before_serving
+async def startup():
+    app.background_task = asyncio.create_task(grpc_stream(token))
+    init_cyoda(token)
+
+@app.after_serving
+async def shutdown():
+    app.background_task.cancel()
+    await app.background_task
+
+#put_application_code_here
 
 if __name__ == '__main__':
     app.run()
--- cyoda_template_project/entity/workflow.py
+++ cyoda_template_project/entity/workflow.py
@@ -1,14 +1,15 @@
 import inspect
+from entity.energy_consumption_data.workflow import workflow as workflow
 
-from entity.entity_template.workflow import workflow
+workflows = [workflow]
 
-process_dispatch = {
-}
-for name, func in inspect.getmembers(workflow, inspect.isfunction):
-    # Optionally, filter out private or unwanted functions
-    if not name.startswith("_"):
-        process_dispatch[name] = func
+process_dispatch = {}
 
+# Iterate over each workflow module and collect functions
+for workflow in workflows:
+    for name, func in inspect.getmembers(workflow, inspect.isfunction):
+        if not name.startswith("_"):
+            process_dispatch[name] = func
 
 def process_event(token, data, processor_name):
     meta = {"token": token, "entity_model": "ENTITY_PROCESSED_NAME", "entity_version": "ENTITY_VERSION"}
